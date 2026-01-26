3c47dd121d055cfc33541732bd2f7e24924c1dca
olaniyi <ayodelebwj@gmail.com>
2026-01-26 13:19:18 -0500

    updated dbsubgrpnam

--- terraform/db.tf
+++ terraform/db.tf
@@ -19,7 +19,7 @@ resource "aws_db_instance" "postgres" {
   username               = var.db_username
   password               = var.db_password
 
-  db_subnet_group_name   = [aws_db_subnet_group.postgres.name]
+  db_subnet_group_name   = aws_db_subnet_group.postgres.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
   publicly_accessible    = false
