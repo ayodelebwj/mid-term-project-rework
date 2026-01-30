3c4db7aff111f17efed3103ba9e23679f4c22e25
Your NameOlaniyi <ayodelebwj@gmail.com>
2026-01-30 15:02:04 -0500

    pushagain

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -20,3 +20,68 @@ resource "aws_iam_instance_profile" "ssm_profile" {
   name = "backend-ssm-profile"
   role = aws_iam_role.ssm_role.name
 }
+
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+resource "aws_iam_role" "github_actions_ssm" {
+  name = "github-actions-ansible-ssm"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:ayodelebwj/mid-term-project-rework:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "github_actions_ssm_policy" {
+  name = "github-actions-ansible-ssm-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:StartSession",
+          "ssm:SendCommand",
+          "ssm:GetCommandInvocation",
+          "ssm:DescribeInstanceInformation",
+          "ec2:DescribeInstances"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_ssm_policy" {
+  role       = aws_iam_role.github_actions_ssm.name
+  policy_arn = aws_iam_policy.github_actions_ssm_policy.arn
+}
+
