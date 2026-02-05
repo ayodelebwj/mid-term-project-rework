ca351d59e806fa6e19d3ca6f16c1b7110db1b788
olaniyi <ayodelebwj@gmail.com>
2026-02-05 02:39:20 -0500

    push32

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "techbleatweek"
+        bucket = "techbleatbucket"
         key = "env/prod/terraform.tfstate"
         region = "us-east-1"
         encrypt = true
