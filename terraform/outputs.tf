c0be4a6d100f1e849abf33bff6046dd96233a7f0
olaniyi <ayodelebwj@gmail.com>
2026-02-05 20:02:32 -0500

    push69

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@ output "frontend_ip" {
   value = aws_instance.frontend.private_ip
 }
 
+output "frontend_public_ip" {
+  value = aws_instance.frontend.public_ip
+}
+
 output "backend_ip" {
   value = aws_instance.backend.private_ip
 }
